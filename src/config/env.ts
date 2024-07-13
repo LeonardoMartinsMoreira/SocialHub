09273410fe31c6d34889daded112d763f37f548d
Leonardo <martins.moreira.leonardo@escola.pr.gov.br>
2024-07-13 17:40:47 -0300

    second commit

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,7 +2,8 @@
 import { z } from 'zod';
 
 const envSchema = z.object({
-  API_KEY: z.coerce.string(),
+  EXPO_PUBLIC_API_KEY: z.string(),
+  EXPO_PUBLIC_PROJECT_ID: z.string(),
 });
 
 export default envSchema.parse(process.env);
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,13 +1,13 @@
 import axios from 'axios';
-import env from '../env';
+import env from '../config/env';
 
-export const apiKey = env.API_KEY;
+export const apiKey = env.EXPO_PUBLIC_API_KEY;
+export const apiProjectID = env.EXPO_PUBLIC_PROJECT_ID;
 
 export const apiAuth = axios.create({
   baseURL: 'https://identitytoolkit.googleapis.com/v1/',
 });
 
 export const apiFirestore = axios.create({
-  baseURL:
-    'https://firestore.googleapis.com/v1/projects/training-app-codize/databases/(default)/',
+  baseURL: `https://firestore.googleapis.com/v1/projects/${apiProjectID}/databases/(default)/`,
 });
